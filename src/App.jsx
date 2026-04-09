48c8219f5792e5d6aacbeb50daedd9c9f4e3a439
CatalinaChandia <cchandia@methanex.com>
2026-04-09 16:59:11 -0300

    Change webhook URL to local API endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ const WHEEL_COLORS = [
   BRAND.gray,
 ];
 
-const WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzMxQbV7CqqDrHp4CWqgPvNOtGP0_bdXf94bTN0pBZ3xeLvkupCKtXbo5zGtR5Ysehs/exec";
+const WEBHOOK_URL = "/api/notify";
 const DAILY_LIMIT_STORAGE_KEY = "methanex_dropped_objects_daily_limit";
 
 function normalizeParticipantName(name) {
